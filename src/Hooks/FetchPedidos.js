611fe4acb633473365f1ee789c7039d93dd54ce7
William Angelis <willangelis@gmail.com>
2022-07-04 02:03:58 -0300

    feat: :alien: add proxy para api

--- src/Hooks/FetchPedidos.js
+++ src/Hooks/FetchPedidos.js
@@ -1,4 +1,4 @@
 export const getPedidos = () => () =>
-  fetch('https://desafiotecnicosti3.azurewebsites.net/pedido').then((res) =>
-    res.json()
-  );
+  fetch(
+    'https://justcors.com/tl_5ea75a1/https://desafiotecnicosti3.azurewebsites.net/pedido'
+  ).then((res) => res.json());
